067a5c79a833e1aa5ed1d7820b6097fbdaea0083
lucky031401 <lucky3748@gmail.com>
2021-09-18 22:05:09 +0800

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from genericpath import exists
 import streamlit as st
 import datetime
+
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
@@ -8,29 +9,27 @@ import pandas as pd
 import altair as alt
 import os 
 
-x = pd.read_csv( './data/sample/sample.csv',names=["forecast"] )
-y = pd.read_csv( './data/sample/sampleInputData.csv')
+x = pd.read_csv( './data/sample/sample.csv',names=["預測負載"] ,index_col = False)
+y = pd.read_csv( './data/sample/sampleInputData.csv',index_col=False)
+today = pd.read_csv('https://www.taipower.com.tw/d006/loadGraph/loadGraph/data/loadareas.csv',names=["time","north","central","south","east"],index_col = False)
+
+today['當日總負載'] = today.sum(axis=1)*10
+today.drop(["time","north","central","south","east"], axis=1, inplace=True)
 y = pd.DataFrame(y['date'])
 y['date'] = pd.to_datetime(y.date,format = '%Y-%m-%d %H:%M:%S') 
 y['date'] = y['date'].dt.tz_localize('Asia/Shanghai')
-x = pd.concat([x,y],axis=1)
-st.title('forecast')
-st.dataframe(x)
-date_en = st.date_input('預測資料')
-st.write('your selection',date_en)
-s = date_en.strftime('%Y_%m_%d')
-if os.path.exists('./data/loadfueltype/'+s+'.csv'):
-    picked = pd.read_csv('./data/loadfueltype/'+s+'.csv')
-    st.dataframe(picked)
-    print(x)
-else:
-    st.write('目前沒有相關資料，請選擇其他日期')
-st.subheader('sample.csv')
-plot = alt.Chart(x).mark_line().encode(
-    alt.X('hoursminutes(date):T', title='hour of day'),
-    alt.Y('forecast:Q', title='Forecast'),
+x = pd.concat([x,y,today],axis=1)
+st.title('電力負載預測')
+st.subheader('預測資料')
+data = x.melt('date',var_name='數據',value_name='負載量（Mw）')
+plot = alt.Chart(data).mark_line().encode(
+    alt.X('hoursminutes(date):T', title='時間'),
+    y='負載量（Mw）:Q',
+    color='數據:N',
+    tooltip=['hoursminutes(date):T','負載量（Mw）']
 ).properties(
     width=700,
     height=330
 )
+
 st.write(plot)
