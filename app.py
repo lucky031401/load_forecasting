2871add0bf0f8f51b0a6e4fc7f9eac0fe5747f6b
lucky031401 <lucky3748@gmail.com>
2021-09-26 18:18:57 +0800

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from genericpath import exists
 import streamlit as st
 import datetime
-
+from datetime import timedelta
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
@@ -21,6 +21,9 @@ y['date'] = pd.to_datetime(y.date,format = '%Y-%m-%d %H:%M:%S')
 y['date'] = y['date'].dt.tz_localize('Asia/Shanghai')
 x = pd.concat([x,y,today],axis=1)
 st.title('電力負載預測')
+d = st.date_input("請選擇欲觀察曲線",datetime.date.today(),min_value=datetime.date.today(),max_value = datetime.date.today()+timedelta(days=2))
+st.write(d)
+
 st.subheader('預測資料')
 data = x.melt('date',var_name='數據',value_name='負載量（Mw）')
 plot = alt.Chart(data).mark_line().encode(
